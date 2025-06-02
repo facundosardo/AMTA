e97f46454c60e89b1f2972fe43a234069bd5d441
facundosardo <sardomkt@gmail.com>
2025-06-02 13:11:00 -0300

    update code

--- amta.py
+++ amta.py
@@ -14,7 +14,7 @@ TARGET_CITIES = [
     "Bethel", "Bridgeport", "Brookfield", "Danbury", "Darien", "Easton",
     "Fairfield", "Greenwich", "Monroe", "New Canaan", "Newtown", "Norwalk",
     "Redding", "Ridgefield", "Shelton", "Sherman", "Stamford", "Stratford",
-    "Trumbull", "Weston", "Westport", "Wilton", "New Fairfield"
+    "Trumbull", "Weston", "Westport", "Wilton", "New Fairfield", "New Haven"
 ]
 
 # Configure browser options
